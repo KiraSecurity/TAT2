00a18abdd180b5aeb1971ed89c9936abbeaafe76
Flaxo P <jack.f.pas@gmail.com>
2024-06-13 17:02:52 -0400

    updated requirements txt

--- amass_module.py
+++ amass_module.py
@@ -1,13 +1,15 @@
 #Modules should be designed to be independant of one another, include ALL requirements
 #Modules also always need to include the cwd variable, and pending required language should include the correlated PATH and language path
 
-
+#####REQUIRED FOR MODULES######
 import os
 import sys
 domain = input("insert domain you would like to run amass scanner on: ")
 cwd = os.getcwd() 
 os.environ['PATH'] = f'{cwd}/go/bin:' + os.environ['PATH']
 os.environ['GOPATH'] = cwd
+#####REQUIRED FOR MODULES######
+
 print(os.system(f'{cwd}/go/bin/go install -v github.com/owasp-amass/amass/v4/...@master'))
 os.environ['PATH'] = f'{cwd}/bin:' + os.environ['PATH']
-print(os.system(f'{cwd}/bin/amass enum -d {domain}'))
+print(os.system(f'script -q -c " {cwd}/bin/amass enum -d {domain}" amass_subdomains.txt'))
--- framework.py
+++ framework.py
@@ -7,7 +7,7 @@ cwd = os.getcwd()
 ## resets the local space and download golang in local framework directory and sets it (base framework to accept go code)
 print(os.system('go version'))
 print(os.system(f'rm -rf {cwd}/go'))
-print(os.system('wget https://golang.org/dl/go1.22.4.linux-amd64.tar.gz'))
+print(os.system('wget https://golang.org/dl/go1.22.4.linux-amd64.tar.gz')) # need to get code to grab latest golang version
 
 print(os.system(f'tar -C {cwd} -xzf go1.22.4.linux-amd64.tar.gz'))
 
