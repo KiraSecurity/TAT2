3e1bb2a9a28e88c8768813936ef0803b28fba0dc
Flaxo P <jack.f.pas@gmail.com>
2024-06-13 17:03:10 -0400

    modifed space

--- amass_module.py
+++ amass_module.py
@@ -9,7 +9,6 @@ cwd = os.getcwd()
 os.environ['PATH'] = f'{cwd}/go/bin:' + os.environ['PATH']
 os.environ['GOPATH'] = cwd
 #####REQUIRED FOR MODULES######
-
 print(os.system(f'{cwd}/go/bin/go install -v github.com/owasp-amass/amass/v4/...@master'))
 os.environ['PATH'] = f'{cwd}/bin:' + os.environ['PATH']
 print(os.system(f'script -q -c " {cwd}/bin/amass enum -d {domain}" amass_subdomains.txt'))
