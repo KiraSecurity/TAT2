92d678ebcd6ee156ee8d599594431a14c21a1648
Flaxo P <jack.f.pas@gmail.com>
2024-06-13 16:44:35 -0400

    added domain variable for amass modified

--- amass_module.py
+++ amass_module.py
@@ -1,8 +1,13 @@
+#Modules should be designed to be independant of one another, include ALL requirements
+#Modules also always need to include the cwd variable, and pending required language should include the correlated PATH and language path
+
+
 import os
 import sys
+domain = input("insert domain you would like to run amass scanner on: ")
 cwd = os.getcwd() 
 os.environ['PATH'] = f'{cwd}/go/bin:' + os.environ['PATH']
 os.environ['GOPATH'] = cwd
 print(os.system(f'{cwd}/go/bin/go install -v github.com/owasp-amass/amass/v4/...@master'))
 os.environ['PATH'] = f'{cwd}/bin:' + os.environ['PATH']
-print(os.system(f'{cwd}/bin/amass enum -d facebook.com'))
+print(os.system(f'{cwd}/bin/amass enum -d {domain}'))
