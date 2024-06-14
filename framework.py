55d3e2a117d9a00b1884a325f2654e06c9854d5b
Flaxo P <jack.f.pas@gmail.com>
2024-06-14 17:05:42 -0400

    running again we back, new menu feature

--- framework.py
+++ framework.py
@@ -19,29 +19,42 @@ print(os.system('go version'))
 print("finished go lang script acceptance")
 
 def get_module_category(module):
+    all_metadata=[]
     with open(module, 'r') as f:
-        metadata = f.readline().strip()
-        if metadata.startswith("#category:"):
-            return metadata.split(':')[1].strip()
-        else:
-            return "unknown"
+        line=0
+        while line < 2:
+            metadata = f.readline().strip()
+            if metadata.startswith("#category:") or metadata.startswith("#input dependencies:"):
+                all_metadata.append(metadata.split(':')[1].strip())
+                #print(all_metadata)
+                line +=1
+            else:
 
-def categoize_modules(modules):
-    categories= {}
-    for module in modules:
-        category=get_module_category(module)
-        update_module_library(categories,category,module)
-    print(categories)
-    return categories
+                print("incorrect metadata format")
+                continue
 
-def update_module_library(categories,category,module):
-    if category not in categories:
-            categories[category]=[module]
-            print(categories)
+        return all_metadata
+
+def categoize_modules(module,library):
+    #module_info={}
+    
+    metadata=get_module_category(module)
+    update_module_library(library,metadata,module)
+    return library
+
+def update_module_library(library, metadata, module):
+    category = metadata[0]
+    info = metadata[1]
+
+    if category not in library:
+        library[category] = {module: info}
+        print(f"Created new category '{category}' with module '{module}'")
     else:
-        categories[category] = [categories[category]]
-        categories[category].append(module)
-        print(categories)  
+        library[category][module] = info
+        print(f"Added module info for '{module}' in category '{category}'")
+
+    return library
+      
 
 
 
@@ -57,25 +70,51 @@ def get_modules():
 
 ###running modules###
 def execute_modules(modules):
-    for module in modules:
-        subprocess.run(["python3", module], check=True)
+        print(modules)
+        subprocess.run(["python3", modules], check=True)
+
+
+
+
+def menu(library):
+    i=0
+    module_dict={}
+    print('\n'"welcome to TAT2!")
+    print("here are the modules you have loaded:"'\n')
+    for category in library:
+    # Print the first module and its value in the category
+        for module, info in library[category].items():
+            i+=1
+            module_dict[i]=module
+            print(f"\033[92m {i} {module}\033[0m")
+
+    print(module_dict)
+    choice=input("select the tool you would like to run first: ")
+    if choice.isdigit():
+        choice = int(choice)
+        #choice=input("select the tool you would like to run first: ")
+        selector=module_dict[choice]
+        selector = str(selector)
+        execute_modules(selector)
+        print("all done here in menu")
+    
 
 def deploy_framework():
     loaded_modules=get_modules()
-    categories=categoize_modules(loaded_modules)
+    library ={}
+    for module in loaded_modules:
+        library=categoize_modules(module,library)
+    menu(library)
+
     process_flow = [
         "subdomain discovery",
         "url prober",
         "crawler",
         "others"
     ]
-    for category in process_flow:
-        if category in categories:
-            execute_modules(categories[category],)
-
-
-
-
+    # for category in process_flow:
+    #     if category in library:
+    #         execute_modules(library[category],)
 
 
 if __name__ == "__main__":
